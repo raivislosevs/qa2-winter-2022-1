308036a56c139ebe5ce4aaa5f8b50518d22032f5
raivis.losevs <test@test.lv>
2022-11-03 14:10:59 +0200

    clickable object from list

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,15 +1,18 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SeleniumTest {
     private final By ACCEPT_COOKIES_BTN = By.id("onetrust-accept-btn-handler");
-    private final By LATVIA = By.xpath(".//a[contains(@href, 'latvia')]");
+    private final By COUNTRIES = By.xpath(".//a[contains(@href, 'https://www.discovercars.com')]");
     private final By TITLE = By.xpath(".//h1[contains(@class, 'home-h1')]");
 
     @Test
@@ -24,8 +27,9 @@ public class SeleniumTest {
         wait.until(ExpectedConditions.elementToBeClickable(ACCEPT_COOKIES_BTN));
         browser.findElement(ACCEPT_COOKIES_BTN).click();
 
-        wait.until(ExpectedConditions.elementToBeClickable(LATVIA));
-        browser.findElement(LATVIA).click();
+        List<WebElement> countries = browser.findElements(COUNTRIES);
+        wait.until(ExpectedConditions.elementToBeClickable(countries.get(251)));
+        countries.get(251).click();
 
         browser.findElement(TITLE);
         System.out.println(browser.findElement(TITLE).getText());
